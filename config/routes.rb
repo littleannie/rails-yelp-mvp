7d08b0a673046eea4dae92a531cd641dcd9f998a
Anne Ly <anne.ly59@gmail.com>
2022-11-16 16:09:34 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :show, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+un = { name: 'pinzutti', address: '34 rue Marcadet', phone_number: '01 86 78 44 56', category: 'italian' }
+deux = { name: 'Gros Bao', address: 'Canal Saint Martin', phone_number: '06 56 45 89 76', category: 'chinese' }
+trois = { name: 'Le brio', address: '65 rue Marcadet', phone_number: '01 82 70 44 59', category: 'french' }
+quattre = { name: 'Kunitoraya', address: '34 rue Villedo', phone_number: '07 56 43 21 67', category: 'japanese' }
+cinq = { name: 'Mamma Roma', address: '38 rue Oberkampf', phone_number: '07 65 22 10 96', category: 'italian' }
+
+[un, deux, trois, quattre, cinq].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
